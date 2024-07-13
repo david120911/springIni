0d18b1fe84bd976f7e5025fccdb78b01a4b06b51
richard <david120911@gmail.com>
2024-07-13 21:37:28 +0900

    git init

---
+++ src/main/java/com/example/springini/SpringIniApplication.java
@@ -0,0 +1,13 @@
+package com.example.springini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIniApplication.class, args);
+    }
+
+}
